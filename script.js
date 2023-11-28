d4b9d2fa51c6314a0e1da435d5416f595704a803
igor488 <igorgabrieldasilva98@gmail.com>
2023-11-28 02:40:42 -0300

    arumando

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-function changeVideo(videoId) {
-    const videoPlayer = document.getElementById('video-player').getElementsByTagName('iframe')[0];
-    videoPlayer.src = `https://www.youtube.com/embed/${videoId}`;
-}
+
